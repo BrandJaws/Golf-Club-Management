8345da4efbcdad8417010c62a3839f65e3f3b07a
Muhammad Arslan Aslam <mrphool43@gmail.com>
2017-01-28 01:01:19 +0500

    side nav links updated

--- resources/views/admin/__partials/left-nav.blade.php
+++ resources/views/admin/__partials/left-nav.blade.php
@@ -15,16 +15,16 @@
             <li> <a href="{{route('admin.dashboard')}}" > <span class="nav-icon"> <i class="material-icons">&#xe3fc; <span ui-include="'{{asset('assets/images/i_0.svg')}}'"></span> </i> </span> <span class="nav-text">Dashboard</span> </a> </li>
             <li> <a href="{{route('admin.members.members')}}" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Members</span> </a> </li>
             <li> <a href="{{route('admin.reservations.reservations')}}" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Reservations</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Profile / Settings</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Shop</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Profile / Settings</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Shop</span> </a> </li>
             <li> <a href="{{route('admin.segments.segments')}}" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Segments</span> </a> </li>
             <li> <a href="{{route('admin.rewards.rewards')}}" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Offer / Rewards</span> </a> </li>
             <li> <a href="{{route('admin.notifications.notifications')}}" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Notifications</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Social</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Staff</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Trainings</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Coaches</span> </a> </li>
-            <li> <a href="widget.html" > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Leagues</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Social</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Staff</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Trainings</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Coaches</span> </a> </li>
+            <li> <a href="#." > <span class="nav-icon"> <i class="material-icons">&#xe8d2; <span ui-include="'{{asset('assets/images/i_3.svg')}}'"></span> </i> </span> <span class="nav-text">Leagues</span> </a> </li>
           </ul>
         </nav>
       </div>

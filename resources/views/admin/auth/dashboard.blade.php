07f1215ad25c514a386e6b37157da729194a8073
Muhammad Arslan Aslam <mrphool43@gmail.com>
2017-02-09 00:52:29 +0500

    dashboard screen design cleaned

--- resources/views/admin/auth/dashboard.blade.php
+++ resources/views/admin/auth/dashboard.blade.php
@@ -1,86 +1,74 @@
 @extends('admin.__layouts.admin-layout')
 @section('main')
-
 <div id="content" class="app-content box-shadow-z0" role="main">
     <div class="app-header white box-shadow">
-      <div class="navbar"> 
-        <!-- Open side - Naviation on mobile --> 
-        <a data-toggle="modal" data-target="#aside" class="navbar-item pull-left hidden-lg-up"> <i class="material-icons">&#xe5d2;</i> </a> 
-        <!-- / --> 
-        
-        <!-- Page title - Bind to $state's title -->
-        <div class="navbar-item pull-left h5" ng-bind="$state.current.data.title" id="pageTitle"></div>
-        
-        <!-- navbar right -->
-        <ul class="nav navbar-nav pull-right">
-          <li class="nav-item dropdown pos-stc-xs"> <a class="nav-link" href data-toggle="dropdown"> <i class="material-icons">&#xe7f5;</i> <span class="label label-sm up warn">3</span> </a>
-            <div ui-include="'../views/blocks/dropdown.notification.html'"></div>
-          </li>
-          <li class="nav-item dropdown"> <a class="nav-link clear" href data-toggle="dropdown"> <span class="avatar w-32"> <img src="{{asset('assets/images/a0.jpg')}}" alt="..."> <i class="on b-white bottom"></i> </span> </a>
-              <div class="dropdown-menu pull-right dropdown-menu-scale ng-scope">
-                  <a class="dropdown-item" ui-sref="app.inbox.list" href="{{route('admin.profile.profile')}}">
-                      <span>Profile</span>
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" ui-sref="access.signin" href="#/access/signin">Sign out</a>
-              </div>
-          </li>
-          <li class="nav-item hidden-md-up"> <a class="nav-link" data-toggle="collapse" data-target="#collapse"> <i class="material-icons">&#xe5d4;</i> </a> </li>
-        </ul>
-        <!-- / navbar right --> 
-        
-        <!-- navbar collapse -->
-        <div class="search__bar">
-          <!--<div ui-include="'../views/blocks/navbar.form.right.html'"></div>-->
-          <!-- link and dropdown -->
-          <div class="row">
-          	<div class="col-md-11">	
-                <ul class="nav navbar-nav">
-            <li class="nav-item dropdown"> 
-            	<h5>Dashboard</h5>
-            </li>
-          </ul>
-          	</div>
-          </div>
-          <!-- / --> 
+        <div class="navbar">
+            <!-- Open side - Naviation on mobile -->
+            <a data-toggle="modal" data-target="#aside" class="navbar-item pull-left hidden-lg-up"> <i class="material-icons">&#xe5d2;</i> </a>
+            <!-- / -->
+            <!-- Page title - Bind to $state's title -->
+            <div class="navbar-item pull-left h5" ng-bind="$state.current.data.title" id="pageTitle"></div>
+            <!-- navbar right -->
+            <ul class="nav navbar-nav pull-right">
+                <li class="nav-item dropdown pos-stc-xs"> <a class="nav-link" href data-toggle="dropdown"> <i class="material-icons">&#xe7f5;</i> <span class="label label-sm up warn">3</span> </a>
+                    <div ui-include="'../views/blocks/dropdown.notification.html'"></div>
+                </li>
+                <li class="nav-item dropdown"> <a class="nav-link clear" href data-toggle="dropdown"> <span class="avatar w-32"> <img src="../assets/images/a0.jpg" alt="..."> <i class="on b-white bottom"></i> </span> </a>
+                    <div class="dropdown-menu pull-right dropdown-menu-scale ng-scope">
+                        <a class="dropdown-item" ui-sref="app.inbox.list" href="{{route('admin.profile.profile')}}">
+                            <span>Profile</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" ui-sref="access.signin" href="#/access/signin">Sign out</a>
+                    </div>
+                </li>
+                <li class="nav-item hidden-md-up"> <a class="nav-link" data-toggle="collapse" data-target="#collapse"> <i class="material-icons">&#xe5d4;</i> </a> </li>
+            </ul>
+            <!-- / navbar right -->
+
+            <!-- navbar collapse -->
+            <div class="collapse navbar-toggleable-sm" id="collapse">
+                <div class="main-page-heading">
+                    <h3> <span>Dashboard</span></h3>
+                </div>
+            </div>
+            <!-- / navbar collapse -->
         </div>
-        <!-- / navbar collapse --> 
-      </div>
     </div>
-    {{--<div class="app-footer">--}}
-      {{--<div class="p-a text-xs">--}}
-        {{--<div class="pull-right text-muted"> &copy; Copyright <strong>Flatkit</strong> <span class="hidden-xs-down">- Built with Love v1.1.3</span> <a ui-scroll-to="content"><i class="fa fa-long-arrow-up p-x-sm"></i></a> </div>--}}
-        {{--<div class="nav"> <a class="nav-link" href="../">About</a> <span class="text-muted">-</span> <a class="nav-link label accent" href="">Get it</a> </div>--}}
-      {{--</div>--}}
-    {{--</div>--}}
+
     <div ui-view class="app-body" id="view"> 
       
       <!-- ############ PAGE START-->
-      <div class="padding">
-
-
-      </div>
      <div class="padding">
-      <div class="row">
+      <div class="row dashboardQuickView">
       <div class="col-sm-6 col-md-4">
-        <div class="box">
-          <div class="box-body">
-            <div ui-jp="plot" ui-refresh="app.setting.color" ui-options="
-              [{data: 20, label:&#x27;Members&#x27;}, {data: 50, label: &#x27;Guests&#x27;}, {data: 30, label:&#x27;Employees&#x27;}],
-              {
-                series: { pie: { show: true, innerRadius: 0.6, stroke: { width: 0 }, label: { show: true, threshold: 0.05 } } },
-                legend: {backgroundColor: 'transparent'},
-                colors: ['#0cc2aa','#fcc100'],
-                grid: { hoverable: true, clickable: true, borderWidth: 0, color: 'rgba(120,120,120,0.5)' },   
-                tooltip: true,
-                tooltipOpts: { content: '%s: %p.0%' }
-              }
-            " style="height:118px"></div>
+          <div class="box p-a" style="height: 80px;">
+              <div class="pull-left m-r">
+                  <span ui-jp="sparkline" ui-refresh="app.setting.color" ui-options="[40,60], {type:'pie', height:36, sliceColors:['#f1f2f3','#0cc2aa']}" class="sparkline inline"><canvas width="36" height="36" style="display: inline-block; width: 36px; height: 36px; vertical-align: top;"></canvas></span>
+              </div>
+              <div class="clear">
+                  <h4 class="m-a-0 text-md"><a href="">60% <span class="text-sm">Increase</span></a></h4>
+                  <small class="text-muted">38 Shipped.</small>
+              </div>
           </div>
-        </div>
+          {{--<div class="box">--}}
+          {{--<div class="box-body">--}}
+            {{--<div ui-jp="plot" ui-refresh="app.setting.color" ui-options="--}}
+              {{--[{data: 20, label:&#x27;Members&#x27;}, {data: 50, label: &#x27;Guests&#x27;}, {data: 30, label:&#x27;Employees&#x27;}],--}}
+              {{--{--}}
+                {{--series: { pie: { show: true, innerRadius: 0.6, stroke: { width: 0 }, label: { show: true, threshold: 0.05 } } },--}}
+                {{--legend: {backgroundColor: 'transparent'},--}}
+                {{--colors: ['#0cc2aa','#fcc100'],--}}
+                {{--grid: { hoverable: true, clickable: true, borderWidth: 0, color: 'rgba(120,120,120,0.5)' },   --}}
+                {{--tooltip: true,--}}
+                {{--tooltipOpts: { content: '%s: %p.0%' }--}}
+              {{--}--}}
+            {{--" style="height:118px"></div>--}}
+          {{--</div>--}}
+        {{--</div>--}}
       </div>
       <div class="col-sm-6 col-md-4">
-        <div class="box p-a" style="height:150px">
+        <div class="box p-a" style="height: 80px;">
 	          <div class="pull-left m-r">
 	            <span class="w-48 rounded  accent">
 	              <i class="material-icons">&#xE8B4;</i>
@@ -93,7 +81,7 @@
 	        </div>
       </div>
       <div class="col-sm-6 col-md-4">
-        <div class="box p-a" style="height:150px">
+        <div class="box p-a" style="height: 80px;">
 	          <div class="pull-left m-r">
 	            <span class="w-48 rounded primary">
 	              <i class="material-icons">&#xE8D3;</i>
@@ -106,9 +94,9 @@
 	        </div>
       </div>
       </div>
-      </div><!-- padding -->
+
       
-     <div class="padding"> 
+     <div class="">
          <div class="dashboard-tsheet">
              <div class="row">
              	<div class="tsheet-header padd-15">
@@ -136,6 +124,7 @@
              	</div>
              </div>
          </div><!-- dashboard tee sheet -->
+     </div><!-- padding -->
      </div><!-- padding -->
     </div>
   </div>

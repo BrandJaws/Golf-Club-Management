10dc8b7ef6e9364cd6880d6ffe3cd306e3e1ed47
kashif <kashifchishti@msn.com>
2017-02-14 23:49:06 +0500

    updated migration and staffController.php
    Employee model added mutators and accessors for staff permissions

--- config/global.php
+++ config/global.php
@@ -4,5 +4,20 @@ return [
 		'gender' => [ 
 				'male' => 'Male',
 				'female' => 'Female' 
-		] 
+		],
+        'staff'=>[
+            'permissions'=>[
+                'member'=>false,
+                'reservation'=>false,
+                'shop'=>false,
+                'segment'=>false,
+                'beacon'=>false,
+                'offer'=>false,
+                'notification'=>false,
+                'social'=>false,
+                'training'=>false,
+                'coach'=>false,
+                'league'=>false
+            ]
+        ]
 ];

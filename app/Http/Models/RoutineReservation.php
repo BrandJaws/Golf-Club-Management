9d661d23bf3d9135c5d0a7955b8d51a6a6434ad2
Muhammad Salman Abid <salman3k1@gmail.com>
2017-03-16 18:08:53 +0000

    Primilinary new reservation for mobile made functional

--- app/Http/Models/RoutineReservation.php
+++ app/Http/Models/RoutineReservation.php
@@ -4,34 +4,39 @@ namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
 class RoutineReservation extends Model
 {
 
-    protected $fillable = [ 
-			'id',
-			'club_id',
-			'course_id'
-	];
-    
-    public function reservation_time_slots(){
-        return $this->morphMany("App\Http\Models\ReservationTimeSlot","reservation");
-    }
-  
-    
-    public function reservation_players(){
-        return $this->morphMany("App\Http\Models\ReservationPlayer","reservation");
+    protected $fillable = [
+        'id',
+        'club_id',
+        'course_id'
+    ];
+
+    public function reservation_time_slots()
+    {
+        return $this->morphMany("App\Http\Models\ReservationTimeSlot", "reservation");
     }
 
-    public function attachPlayers($players, $parent = 0, $confirmAll = false, $group_size,$reservation_status) {
-        if (is_array ( $players ) && ! empty ( $players )) {
-            foreach ( $players as $player ) {
 
-                $status = ($parent  && $player == $parent) || $player == "guest" ||  $confirmAll ? \Config::get ( 'global.reservation.confirmed' )  :  \Config::get ( 'global.reservation.pending' ) ;
+    public function reservation_players()
+    {
+        return $this->morphMany("App\Http\Models\ReservationPlayer", "reservation");
+    }
 
-                if($player == "guest"){
+    public function attachPlayers($players, $parent, $confirmAll, $group_size, $reservation_status)
+    {
+        if (is_array($players) && !empty ($players)) {
+            foreach ($players as $player) {
+
+                $status = ($parent && $player == $parent) || $player == "guest" || $confirmAll ? \Config::get('global.reservation.confirmed') : \Config::get('global.reservation.pending');
+
+                if ($player == "guest") {
                     ReservationPlayer::create([
                         'reservation_id' => $this->id,
-                        'reservation_type'=>self::class,
+                        'reservation_type' => self::class,
                         'member_id' => 0,
                         'parent_id' => $parent,
                         'group_size' => $group_size,
@@ -39,10 +44,10 @@ class RoutineReservation extends Model
                         'reservation_status' => $reservation_status
                     ]);
 
-                }else{
+                } else {
                     ReservationPlayer::create([
                         'reservation_id' => $this->id,
-                        'reservation_type'=>self::class,
+                        'reservation_type' => self::class,
                         'member_id' => $player,
                         'parent_id' => $parent,
                         'group_size' => $group_size,
@@ -55,15 +60,122 @@ class RoutineReservation extends Model
         }
 
     }
-    
-    public function attachTimeSlot($timeSlot) {
-	
-                ReservationTimeSlot::create([ 
-                            'reservation_id' => $this->id,
-                            'reservation_type'=>self::class,
-                            'time_start' => $timeSlot
-                ]);
-                       
+
+    public function attachTimeSlot($timeSlot)
+    {
+
+        ReservationTimeSlot::create([
+            'reservation_id' => $this->id,
+            'reservation_type' => self::class,
+            'time_start' => $timeSlot
+        ]);
+
+    }
+
+    public function updateReservationStatusesForAReservation()
+    {
+        if ($this->reservation_groups) {
+
+            foreach ($this->reservation_groups as $group) {
+                //Sync number of remaining players to group size
+                if (count($group->players) < $group->group_size) {
+                    foreach ($group->players as $player) {
+                        if ($player->group_size != count($group->players)) {
+                            $player->group_size = count($group->players);
+                            $player->save();
+                        }
+                    }
+
+                }
+
+                //change status to reserved or waiting from pending based on the number of members who have accepted
+                if ($this->playersInAGroupWithResponseStatusConfirmed($group) >= $group->group_size) {
+                    for ($x = 0; $x < $group->group_size; $x++) {
+                        foreach ($group->players as $player) {
+                            if ($player->reservation_status == \Config::get('global.reservation.pending_reserved')) {
+                                $player->reservation_status = \Config::get('global.reservation.reserved');
+                                $player->save();
+                            } else if ($player->reservation_status == \Config::get('global.reservation.pending_waiting')) {
+                                $player->reservation_status = \Config::get('global.reservation.waiting');
+                                $player->save();
+                            }
+                        }
+
+                    }
+
+                }
+
+                //promote group from waiting to reserved if eligible
+                $sumOfGroupSizesReserved = $this->sumOfGroupSizes('reserved');
+
+                if (
+                     ($group->reservation_status == \Config::get('global.reservation.pending_waiting') ||
+                      $group->reservation_status == \Config::get('global.reservation.waiting') ) &&
+                      $sumOfGroupSizesReserved < 4 &&
+                      $this->group_size <= (4-$sumOfGroupSizesReserved)
+
+                ){
+                    foreach ($group->players as $player) {
+                        if ($player->reservation_status == \Config::get('global.reservation.pending_waiting')) {
+                            $player->reservation_status = \Config::get('global.reservation.pending_reserved');
+                            $player->save();
+                        } else if ($player->reservation_status == \Config::get('global.reservation.waiting')) {
+                            $player->reservation_status = \Config::get('global.reservation.reserved');
+                            $player->save();
+                        }
+                    }
+                }
+
+
+            }
+            //dd($this->reservation_groups);
+
+
+
+        }
+    }
+
+
+    public function playersInAGroupWithResponseStatusConfirmed($group)
+    {
+        $confirmedPlayers = 0;
+        foreach ($group->players as $player) {
+            if ($player->response_status == \Config::get('global.reservation.confirmed')) {
+                $confirmedPlayers++;
+            }
+        }
+
+        return $confirmedPlayers;
+    }
+
+    public function playersInAGroupWithResponseStatusPending($group)
+    {
+        $pendingPlayers = 0;
+        foreach ($group->players as $player) {
+            if ($player->response_status == \Config::get('global.reservation.pending')) {
+                $pendingPlayers++;
+            }
+        }
+
+        return $pendingPlayers;
+    }
+
+    public function getGroupByParentId($parent_id)
+    {
+
+        if ($this->reservation_groups) {
+            $groupFound = null;
+            foreach ($this->reservation_groups as $group) {
+                if ($group->parent_id == $parent_id) {
+                    $groupFound = $group;
+                    break;
+                }
+
+            }
+            return $groupFound;
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -76,25 +188,27 @@ class RoutineReservation extends Model
      * -reserved
      * -both
      */
-    public function sumOfGroupSizes($reservation_status){
+    public function sumOfGroupSizes($reservation_status)
+    {
 
-            if($this->reservation_groups){
-                $sum = 0;
-                foreach($this->reservation_groups as $group){
-                    if($reservation_status == "reserved"){
-                        if($group->reservation_status == \Config::get ( 'global.reservation.reserved' ) ||
-                            $group->reservation_status == \Config::get ( 'global.reservation.pending_reserved' )){
-                            $sum += $group->group_size;
-                        }
-                    }else if($reservation_status == "both"){
+        if ($this->reservation_groups) {
+            $sum = 0;
+            foreach ($this->reservation_groups as $group) {
+                if ($reservation_status == "reserved") {
+                    if ($group->reservation_status == \Config::get('global.reservation.reserved') ||
+                        $group->reservation_status == \Config::get('global.reservation.pending_reserved')
+                    ) {
                         $sum += $group->group_size;
                     }
-
+                } else if ($reservation_status == "both") {
+                    $sum += $group->group_size;
                 }
-                return $sum;
-            }else{
-                return null;
+
             }
+            return $sum;
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -104,88 +218,82 @@ class RoutineReservation extends Model
      * finds a reservation by id and then processes it to make groups based on parent, ordering time etc criteria
      * to facilitate the process of reservations
      */
-    public static function findAndGroupReservationForReservationProcess($reservation_id){
+    public static function findAndGroupReservationForReservationProcess($reservation_id)
+    {
 
-            $reservation = RoutineReservation::where('id',$reservation_id)->with(['reservation_players' => function ($query) {
-                                                                                        $query->orderBy('created_at', 'asc');
-                                                                                        $query->orderBy('updated_at', 'asc');
-                                                                                    }])->first();
-            $reservation_groups = [];
+        $reservation = RoutineReservation::where('id', $reservation_id)->with(['reservation_players' => function ($query) {
+            $query->orderBy('parent_id', 'asc');
+            $query->orderBy('response_status', 'asc');
+            $query->orderBy('updated_at', 'asc');
+        }])->first();
+        $reservation_groups = [];
 
-            if($reservation && $reservation->reservation_players){
+        if ($reservation && $reservation->reservation_players) {
 
-                $groupCount = -1;
-                $tempParentId = 0;
-                $totalRequestedReservationSlots = 0;
-
-                 foreach($reservation->reservation_players as $reservationPlayer){
-                     if($tempParentId != $reservationPlayer->parent_id ){
-                         $groupCount++;
-                         $tempParentId = $reservationPlayer->parent_id;
-                         $reservation_groups[$groupCount] = new \stdClass();
-                         $reservation_groups[$groupCount]->parent_id = $reservationPlayer->parent_id;
-                         $reservation_groups[$groupCount]->reservation_status = $reservationPlayer->reservation_status;
-                         $reservation_groups[$groupCount]->group_size = $reservationPlayer->group_size;
-                         $reservation_groups[$groupCount]->players = [];
-                         $totalRequestedReservationSlots += $reservationPlayer->group_size;
+            $groupCount = -1;
+            $tempParentId = 0;
 
 
-                     }
-                     $reservation_groups[$groupCount]->players[] = $reservationPlayer;
+            foreach ($reservation->reservation_players as $reservationPlayer) {
+                if ($tempParentId != $reservationPlayer->parent_id) {
+                    $groupCount++;
+                    $tempParentId = $reservationPlayer->parent_id;
+                    $reservation_groups[$groupCount] = new \stdClass();
+                    $reservation_groups[$groupCount]->parent_id = $reservationPlayer->parent_id;
+                    $reservation_groups[$groupCount]->reservation_status = $reservationPlayer->reservation_status;
+                    $reservation_groups[$groupCount]->group_size = $reservationPlayer->group_size;
+                    $reservation_groups[$groupCount]->created_at = $reservationPlayer->created_at;
+                    $reservation_groups[$groupCount]->players = [];
 
-                 }
+                }
+                $reservation_groups[$groupCount]->players[] = $reservationPlayer;
+
             }
-            $reservation->reservation_groups = $reservation_groups;
-            $reservation->requested_reservation_slots = $totalRequestedReservationSlots;
-            return $reservation;
+        }
+        usort($reservation_groups, function ($a, $b) {
 
+            return $a->created_at->diffInSeconds($b->created_at);
+        });
+        $reservation->reservation_groups = $reservation_groups;
 
+        return $reservation;
 
 
     }
-    
-    
-   
-    public function modifyReservationObjectForReponseOnCRUDOperations(){
-          $players = [];
-          $this->load('reservation_players.member','reservation_time_slots');
-          foreach($this->reservation_players as $player){
-              $players[] = ["reservation_player_id"=>$player->id,
-                            "member_id"=>$player->member != null ? $player->member->id : 0,
-                            "member_name"=>$player->member != null ? $player->member->firstName." ".$player->member->lastName : 'Guest'
-                           ];
-          }
-          $this->reservation_id = $this->id;
-          $this->reservation_type = RoutineReservation::class;
-          $this->players = $players;
-          
-          $reserved_at = null;
-          $timeSlots = [];
-          foreach($this->reservation_time_slots as $timeSlot){
-             if($reserved_at == null){
-                 $reserved_at = Carbon::parse($timeSlot->time_start)->toDateString(); 
-             }
-             $timeSlots[] = Carbon::parse($timeSlot->time_start)->format('h:i A' );
-          }
-         
-          $this->reserved_at = $reserved_at;
-          $this->timeSlots = $timeSlots;
-          
-          unset($this->id);
-          unset($this->nextJobToProcess);
-          unset($this->reservation_players);
-          
-          
-          
-         
+
+
+    public function modifyReservationObjectForReponseOnCRUDOperations()
+    {
+        $players = [];
+        $this->load('reservation_players.member', 'reservation_time_slots');
+        foreach ($this->reservation_players as $player) {
+            $players[] = ["reservation_player_id" => $player->id,
+                "member_id" => $player->member != null ? $player->member->id : 0,
+                "member_name" => $player->member != null ? $player->member->firstName . " " . $player->member->lastName : 'Guest'
+            ];
+        }
+        $this->reservation_id = $this->id;
+        $this->reservation_type = RoutineReservation::class;
+        $this->players = $players;
+
+        $reserved_at = null;
+        $timeSlots = [];
+        foreach ($this->reservation_time_slots as $timeSlot) {
+            if ($reserved_at == null) {
+                $reserved_at = Carbon::parse($timeSlot->time_start)->toDateString();
+            }
+            $timeSlots[] = Carbon::parse($timeSlot->time_start)->format('h:i A');
+        }
+
+        $this->reserved_at = $reserved_at;
+        $this->timeSlots = $timeSlots;
+
+        unset($this->id);
+        unset($this->nextJobToProcess);
+        unset($this->reservation_players);
+
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
 }

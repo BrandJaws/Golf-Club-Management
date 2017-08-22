49b08065c6a6014f6cbb7fb6118be688518eb132
Muhammad Salman Abid <salman3k1@gmail.com>
2017-08-22 16:04:44 +0000

    Fixed minor bug in confirmation popup vue component

--- resources/views/admin/__vue_components/popups/confirmation-popup.blade.php
+++ resources/views/admin/__vue_components/popups/confirmation-popup.blade.php
@@ -1,31 +1,33 @@
+<template xmlns="http://www.w3.org/1999/html" id="closeModalTemplate">
+    <div v-on:click="closeModal($event)" >
+        <div class="modal fade animate in closePopup" data-backdrop="false" style="display: block;">
+            <div class="modal-dialog  modal-lg fade-down" id="animate" ui-class="fade-down">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close closePopup" aria-label="Close"><span aria-hidden="true" class="closePopup">×</span></button>
+                        <h5 class="modal-title">Cancel Reservation</h5>
+                    </div>
+                    <div class="alert alert-danger" v-if="errorMessage != '' ">
+                        @{{ errorMessage }}
+                    </div>
+                    <div class="modal-body text-center p-lg">
+                        <p class="text-center">@{{ popupMessage }}</p>
+                    </div>
+                    <div class="modal-footer text-center">
+                        <button type="button" class="btn primary" v-on:click="yesSelected">Yes</button>
+                        &nbsp;&nbsp;
+                        <button  type="button" class="closePopup btn btn-outline b-primary text-primary" >No</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-backdrop fade in closePopup"></div>
+    </div>
+</template>
 <script>
 
     Vue.component('confirmation-popup', {
-        template: `
-        <div @click="closeModal($event)">
-               <div class="modal fade animate in closePopup" data-backdrop="false" style="display: block;">
-                  <div class="modal-dialog  modal-lg fade-down" id="animate" ui-class="fade-down">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                          <button type="button" class="close closePopup" aria-label="Close"><span aria-hidden="true" class="closePopup">×</span></button>
-                            <h5 class="modal-title">Cancel Reservation</h5>
-                      </div>
-                      <div class="alert alert-danger" v-if="errorMessage != '' ">
-                                    @{{ errorMessage }}
-                     </div>
-      <div class="modal-body text-center p-lg">
-        <p class="text-center">@{{ popupMessage }}</p>
-                      </div>
-                      <div class="modal-footer text-center">
-                        <button type="button" class="btn primary" @click="yesSelected">Yes</button>
-                        &nbsp;&nbsp;
-                        <button  type="button" class="closePopup btn btn-outline b-primary text-primary" >No</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="modal-backdrop fade in closePopup"></div>
-            `,
+        template: "#closeModalTemplate",
     props: [
 
             'popupMessage',

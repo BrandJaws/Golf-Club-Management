d65b229d53e872746ec95e17d15fffb2572527e5
Muhammad Salman Abid <salman3k1@gmail.com>
2017-09-05 16:45:09 +0000

    Fixed category heading bug in vue component for shop

--- resources/views/admin/__vue_components/shop/shop.blade.php
+++ resources/views/admin/__vue_components/shop/shop.blade.php
@@ -83,7 +83,7 @@
                                 <div class="">
                                     <div class="col-md-4">
                                         <div class="inner-page-heading text-left">
-                                            <h3>@{{ categoriesData[selectedCategoryIndex].name }}</h3>
+                                            <h3>@{{ selectedCategoryIndex != null ? categoriesData[selectedCategoryIndex].name : 'No Category Selected' }}</h3>
                                         </div>
                                     </div>
                                     <div class="col-md-8">
